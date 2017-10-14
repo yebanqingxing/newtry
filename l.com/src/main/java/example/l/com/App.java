dd819f48a6d24d559328bb29c320e88142b69c4a
李少博 <lishaobo13@gmail.com>
2017-10-14 09:56:04 +0800

    周六

--- l.com/src/main/java/example/l/com/App.java
+++ l.com/src/main/java/example/l/com/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!,today is the day" );
+        System.out.println("today is sunsday!");
     }
 }
